9222cf7ba491abbf8a05d9fd349ce4c6c4b43622
Valentijn Nieman <valentijnnieman@gmail.com>
2017-05-01 10:08:06 +0200

    more allowedorigin tests

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
+			return origin == "https://valentijnnieman.github.io"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
